540a624e9c9c080edae55c11cb29f654058ff020
sreasgop <sreasgop@gmail.com>
2024-04-01 04:31:59 +0530

    Initial Commit

---
+++ Source Codes/03_JavaScript/index.js
@@ -0,0 +1,12 @@
+// JavaScript code is inserted in HTML document using the Script Tag.
+
+alert("This is the begining of something great!")
+name = prompt("Enter your name: ");
+age = prompt("Enter your age: ");
+alert("Hello "+name+" you were born at "+(2023-age)+".");
+
+// Data Types in JavaScript
+
+/* This 
+is a multi line comment. 
+Javascript is fun!> */
